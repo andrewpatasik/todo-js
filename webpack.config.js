f62add072b1ebdbb68cc573157e29c5efd8508d8
andrewpatasik <andrew.yehezkiel17@gmail.com>
2020-11-17 12:00:04 +0800

    setup git repo to existing project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },  
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              inputPath: 'assets',
+              outputPath: 'assets'
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    })
+  ]
+};
